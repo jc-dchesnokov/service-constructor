4150e7197ba761bba1566bebedd1671a78a006e2
Dmitriy Chesnokov <dima3141592@gmail.com>
2015-10-07 16:50:15 +0300

    work

--- src/Application/ServiceConstructorBundle/Controller/MainController.php
+++ src/Application/ServiceConstructorBundle/Controller/MainController.php
@@ -6,8 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class MainController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('ApplicationServiceConstructorBundle:Main:index.html.twig', array('name' => $name));
+        return $this->render('ApplicationServiceConstructorBundle:Main:index.html.twig', []);
     }
 }
